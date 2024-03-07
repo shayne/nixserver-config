2b2f0e95dabe50607bec2f09c255633f61865237
shayne <79330+shayne@users.noreply.github.com>
2024-03-07 16:40:06 -0500

    add secrets!

--- containers/ombi/configuration.nix
+++ containers/ombi/configuration.nix
@@ -1,3 +1,23 @@
+{ pkgs, ... }: let
+  # TODO(shayne): support "ombi.hostname"/nested secrets
+  hostname = pkgs.readSecret "ombi_hostname";
+  tunnelId = pkgs.readSecret "ombi_tunnel_id";
+in
 {
-  # Right now our initial configuration.nix is empty
+  services.ombi.enable = true;
+  services.ombi.user = "nixserver-service";
+  services.ombi.group = "nixserver-service";
+
+  services.cloudflared.enable = true;
+  services.cloudflared.tunnels = {
+    "${tunnelId}" = {
+      credentialsFile = "${toString (pkgs.writeSecret "ombi_credentialsFile")}";
+      default = "http_status:404";
+      ingress = {
+        "${hostname}" = {
+          service = "http://localhost:5000";
+        };
+      };
+    };
+  };
 }
--- containers/ombi/flake.nix
+++ containers/ombi/flake.nix
@@ -2,9 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixserver.url = "https://releases.nixserver.io/latest/nixosModules";
+    secrets.url = "path:/home/shayne/nixserver-config/secrets";
   };
 
-  outputs = { self, nixpkgs, nixserver }: {
+  outputs = { self, nixpkgs, nixserver, secrets }: {
     nixosConfigurations.container = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -12,7 +13,7 @@
         ./configuration.nix
       ];
       specialArgs = {
-        inherit nixserver;
+        inherit nixserver secrets;
         containerPath = ./.;
       };
     };
