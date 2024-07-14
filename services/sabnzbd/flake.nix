fb6246bac69f3095902b1bd2743b24923b1584b6
shayne <github@shaynes.email>
2024-07-14 11:09:14 -0400

    Update flake.nix

--- services/sabnzbd/flake.nix
+++ services/sabnzbd/flake.nix
@@ -1,18 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    secrets.url = "github:nixserver-io/secrets-placeholder";
     nixserver.url = "https://releases.nixserver.io/latest/nixosModules";
+    nixserver.inputs.nixos-unstable.follows = "nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, nixserver }: {
+  outputs = { self, nixpkgs, nixserver, secrets, ... }@inputs: {
     nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      system = "aarch64-linux";
       modules = [
         nixserver.nixosModules.docker
         ./configuration.nix
       ];
       specialArgs = {
-        inherit nixserver;
+        inherit nixserver secrets;
         servicePath = ./.;
       };
     };
