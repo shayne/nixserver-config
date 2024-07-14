8d66b65dcf37b22883dc94a5fc28bd4edee67c1e
shayne <github@shaynes.email>
2024-07-14 11:11:07 -0400

    Update flake.nix

--- services/sabnzbd/flake.nix
+++ services/sabnzbd/flake.nix
@@ -9,7 +9,7 @@
 
   outputs = { self, nixpkgs, nixserver, secrets, ... }@inputs: {
     nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
+      system = "x86_64-linux";
       modules = [
         nixserver.nixosModules.docker
         ./configuration.nix
