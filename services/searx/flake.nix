13cce563c093cb236ab7c22aa56a721c5aba65af
shayne <github@shaynes.email>
2024-07-14 09:58:24 -0400

    Update flake.nix

--- services/searx/flake.nix
+++ services/searx/flake.nix
@@ -1,8 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     secrets.url = "github:nixserver-io/secrets-placeholder";
     nixserver.url = "https://releases.nixserver.io/latest/nixosModules";
+    nixserver.inputs.nixos-unstable.follows = "nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs, nixserver, secrets }: {
