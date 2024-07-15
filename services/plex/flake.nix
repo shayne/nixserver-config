86cefea7b0a137b307d4ff4d31131adda9c527ff
shayne <github@shaynes.email>
2024-07-14 20:29:59 -0400

    Update flake.nix

--- services/plex/flake.nix
+++ services/plex/flake.nix
@@ -1,11 +1,13 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     secrets.url = "github:nixserver-io/secrets-placeholder";
     nixserver.url = "https://releases.nixserver.io/latest/nixosModules";
+    nixserver.inputs.nixos-unstable.follows = "nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, nixserver, secrets }: {
+  outputs = { self, nixpkgs, nixserver, secrets, ... }@inputs: {
     nixosConfigurations.container = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
